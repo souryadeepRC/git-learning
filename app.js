56eedfb2568faa69a60b425b6efa29e63dfeca8c
souryadeepRC <thecreativeworldforall@gmail.com>
2024-08-21 23:41:29 +0530

    added address in app file

---
+++ app.js
@@ -0,0 +1,3 @@
+const getAddress = () => {
+  return "Address";
+};
--- index.js
+++ index.js
@@ -2,6 +2,3 @@ const getName = (name, title) => {
   if (!name) return "";
   return `Hello ${name} ${title}. Welcome`;
 };
-const getAddress = () => {
-  return "Address";
-};
